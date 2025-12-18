149903811651e9f94af96c72eb7f80dfda9d4e79
denisgerad <dennis.gd@gmail.com>
2025-12-18 04:48:15 +0000

    Fix escaped tab characters in api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,15 +2,15 @@ import axios from 'axios'
 
 
 const API = axios.create({ 
-\tbaseURL: import.meta.env.VITE_API_BASE || 'http://localhost:5000/api',
-\twithCredentials: true // Enable sending cookies with requests
+	baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:5000/api',
+	withCredentials: true // Enable sending cookies with requests
 })
 
 
 // No longer need to manually set Authorization header
 // The httpOnly cookie will be sent automatically
 API.interceptors.request.use((config) => {
-\treturn config
+	return config
 })
 
 
